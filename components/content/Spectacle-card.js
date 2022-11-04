7a224420996689b61f446214e89fdb2847cba311
Garance Wetzel <garance.wetzel@gmail.com>
2022-11-04 09:57:05 +0100

    suppr next

--- components/content/Spectacle-card.js
+++ components/content/Spectacle-card.js
@@ -17,21 +17,19 @@ export default function Card ({item, tag = false, key}) {
       <a className='h-full group'>
         <article className='h-full flex flex-col justify-between'>
           <main className='mb-4 relative'>
-            {tag && renderTagName(item.tags) && (
+            {/* {tag && renderTagName(item.tags) && (
               <Tag 
                 color={renderTagName(item.tags) === "En tournée" ? "cerulean" : "turquoiseGreen"} 
                 className="absolute right-0 z-10 mb-1">
                   {renderTagName(item.tags)}
               </Tag>
               )
-            }
-            <div className='relative h-[10rem] lg:h-[16rem] w-full overflow-hidden mb-3 opacity-80 group-hover:opacity-100 transition-opacity duration-100'>
-              <Image 
+            } */}
+            <div className='relative aspect-square overflow-hidden mb-3' >
+              <img 
                 src={item.feature_image} 
                 alt={item.title} 
-                layout="fill"
-                className='object-cover object-center min-w-full'>
-              </Image> 
+                className='object-cover object-center min-w-full min-h-full scale-100 transition duration-500 group-hover:scale-105'/>
             </div>
             <h3 className='text-cerulean-800 text-xl lg:text-2xl mb-3'>{item.title}</h3>
             <p className='text-gray-600'>{item.excerpt}</p>
